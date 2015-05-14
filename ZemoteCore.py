4b79c58ebf0946478cf11628728b4596f8682050
Cameron Lai <cameronjml2@gmail.com>
2015-05-14 23:36:06 +0800

    Update license info

--- ZemoteCore.py
+++ ZemoteCore.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python
-# Zemote
-# (C) Copyright 2014 Cameron Lai
-#
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the GNU Lesser General Public License
-# (LGPL) version 3.0 which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/lgpl-3.0.html
-#
-# Zemote is distributed in the hope that it will be useful,
+# This file is part of ZemoteHost.
+# 
+# ZemoteHost is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ZemoteHost is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# Lesser General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ZemoteHost.  If not, see <http://www.gnu.org/licenses/>.
 
 import serial
 import glob
